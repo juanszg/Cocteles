17e78178c632f7138534abf75894aea33665cc8a
juanszg <juanszg772@gmail.com>
2024-08-06 13:45:40 -0600

    modifique tequila, cartas y titulo

---
+++ src/pages/colores.js
@@ -0,0 +1,17 @@
+alert('cocteles maximum premium 🥇🥇🥇🥇🥇');
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const title = document.getElementById('animated-title');
+    const text = title.innerText;
+    title.innerHTML = '';
+  
+    text.split('').forEach((char, index) => {
+      const span = document.createElement('span');
+      span.innerText = char;
+      span.style.animationDelay = `${index * 1}s`;
+      title.appendChild(span);
+    });
+  });
+
+  
